b54f8e49a905875438ab59bc74555cd60dcb96ac
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2024-01-02 15:58:49 +0200

    Update LowerCase.java

--- LowerCase.java
+++ LowerCase.java
@@ -1,4 +1,3 @@
-/** String processing exercise 1. */
 public class LowerCase {
     public static void main(String[] args) {  
         String str = args[0];
@@ -10,8 +9,20 @@ public class LowerCase {
     * except that all the upper-case letters are converted to lower-case letters.
     * Non-letter characters are left as is.
     */
-    public static String lowerCase(String s) {
-        // Replace the following statement with your code
-        return null;
+    public static String lowerCase(String str) {
+        String lowstr = "" ;
+        int a = str.length();
+        int i = 0 ;
+        while ( i < a ) {
+            if ( str.charAt(i) >= 'A' && str.charAt(i) <= 'Z' ) {
+                int decimalValue = 32 + (int) str.charAt(i);
+                char character = (char) decimalValue;
+                lowstr += character ;
+            } else {
+                lowstr += str.charAt(i) ;
+            } 
+            i ++; 
+        }
+        return lowstr;
     }
 }
