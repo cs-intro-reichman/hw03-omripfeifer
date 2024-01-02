9bee98774d6b84f14066b43ba42b119688f006a4
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2024-01-02 20:54:32 +0200

    Update Calendar0.java

--- Calendar0.java
+++ Calendar0.java
@@ -7,7 +7,8 @@ public class Calendar0 {
 	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
 	public static void main(String args[]) {
 		int year = Integer.parseInt(args[0]);
-		isLeapYearTest(year);
+
+       	isLeapYearTest(year);
 		nDaysInMonthTest(year);
 	}
 		 
@@ -22,22 +23,41 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+	for ( int month = 1; month <= 12; month++){
+	int days = nDaysInMonth(month, isLeapYear(year));
+	System.out.println(" Month " + month + " has " + days + " days") ;
+	  }
 	}
+            
+        
+	
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+        return  ((year % 400 ) == 0 ) || (((year % 4 ) == 0 ) &&((year % 100) != 0)) ;
 	}
 	 
 	// Returns the number of days in the given month and year.
 	// April, June, September, and November have 30 days each.
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
-	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
+	public static int nDaysInMonth(int month, boolean isLeapYear) {
+		if(isLeapYear){
+          if ( month == 2){
+              return 29 ;
+        } else if ( month == 4 || month == 6 || month == 9 || month == 11){
+            return 30 ;
+        } else {
+            return 31 ;
+        }
+        } else {
+                if ( month == 2){
+              return 28 ;
+        } else if ( month == 4 || month == 6 || month == 9 || month == 11){
+           return 30 ;
+        } else {
+            return 31 ;
+        }
+        }
 	}
 }
-
